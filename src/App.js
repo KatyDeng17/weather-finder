a4a4419ec0fe9e26e3568bf66a47b1346e7f84df
Katy Deng <deng2017.xq@gmail.com>
2018-10-09 16:28:41 -0700

    adding api in file App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import Titles from "./components/Titles";
-import Form from "./components/Form";
-import Weather from "./components//Weather"; 
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components//Weather'; 
+
+
+const API_KEY = 'b3a0e126658bea71b0c925f67e289995';
 
 class App extends React.Component{
+  getWeather = async ()=>{
+    const api_call = await fetch(` http://api.openweathermap.org/data/2.5/weather?q=irvine,united%20state}&appid=${API_KEY}&units=imperial`);
+
+    const data = await api_call.json();
+    console.log(data);
+}
  render(){
    return(
      //return JSX 
--- src/components/Form.js
+++ src/components/Form.js
@@ -10,4 +10,4 @@ class Form extends React.Component{
   }
 }
 
-export default Form;
+export default Form
