80c78e30d0cccc4dec3b34f488deb9a169e2f502
Katy Deng <deng2017.xq@gmail.com>
2018-10-10 00:18:21 -0700

    adding state and setState

--- src/App.js
+++ src/App.js
@@ -7,21 +7,37 @@ import Weather from './components//Weather';
 const API_KEY = 'b3a0e126658bea71b0c925f67e289995';
 
 class App extends React.Component{
+  //adding states
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined, 
 
+  }
   //adding getWeather module  
   getWeather = async (event)=>{
     event.preventDefault();
     //getting the value from the input 
     const city = event.target.elements.city.value; 
     const country = event.target.elements.country.value;
-
-  
     const api_call = await fetch(` http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
 
     const data = await api_call.json();
     console.log(data);
-    // console.log(city);
-    // console.log(country);
+
+    //setState;
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country:data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+     })
+  
 }
 
 
