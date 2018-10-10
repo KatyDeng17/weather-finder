77242f89785bd17c7bd2afb19f9594fef8094778
Katy Deng <deng2017.xq@gmail.com>
2018-10-10 01:11:00 -0700

    adding conditions for the data to show under Weather.js component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,12 +4,13 @@ import React from 'react';
 class Weather extends React.Component{
   render(){
     return(
+      // adding conditions for the data to show 
       <div>
-        <p>temperature: {this.props.temperature}</p>
-        <p>city: {this.props.city}</p>
-        <p>country: {this.props.country}</p>
-        <p>humidity: {this.props.humidity}</p>
-        <p>description: {this.props.description}</p>
+        {this.props.temperature && <p>temperature: {this.props.temperature} F</p>}
+        {this.props.city &&  <p>city: {this.props.city}</p>}   
+        {this.props.country &&  <p>country: {this.props.country}</p>}     
+        {this.props.humidity &&  <p>humidity: {this.props.humidity}</p>}  
+        {this.props.description &&  <p>description: {this.props.description}</p>}   
       </div>
     )
   }
