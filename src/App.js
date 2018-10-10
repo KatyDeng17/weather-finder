a775dc358838f649cc35221978678a667bb2d808
Katy Deng <deng2017.xq@gmail.com>
2018-10-09 23:52:56 -0700

    adding preventDefault() to show the data on console'

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ const API_KEY = 'b3a0e126658bea71b0c925f67e289995';
 class App extends React.Component{
 
   //adding getWeather module  
-  getWeather = async ()=>{
+  getWeather = async (event)=>{
+    event.preventDefault();
     const api_call = await fetch(` http://api.openweathermap.org/data/2.5/weather?q=irvine,united%20state}&appid=${API_KEY}&units=imperial`);
 
     const data = await api_call.json();
@@ -22,7 +23,8 @@ class App extends React.Component{
      //return JSX 
      <div>
        <Titles />
-       <Form getWeather={this.getWeather}/>
+       
+       <Form getWeather={this.getWeather}/> 
        <Weather />
      </div>
 
